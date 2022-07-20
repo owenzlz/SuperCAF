24daf8ce31203989469d3dbf055cf94275077f6a
Lingzhi Zhang <lingzzha@Kates-MacBook-Pro.local>
2022-07-20 16:25:57 -0700

    updated

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,6 +1,6 @@
 pip install gdown
 mkdir datasets
 cd datasets
-# gdown https://drive.google.com/uc?id=1QNvUzQajYs-_VxPRtq-Ze3RBP5xsuUJO
-# unzip PA4Inpaint_subset.zip
-# rm PA4Inpaint_subset.zip
+gdown https://drive.google.com/uc?id=1R7sDZzz25xKello36V-S4-xkw4pjCX6O
+unzip SuperCAF_testset.zip
+rm SuperCAF_testset.zip
