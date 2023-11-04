571ad8147c200e311bdf9295c949f67e52d7f620
owenzlz <32859275+owenzlz@users.noreply.github.com>
2023-11-04 12:58:12 -0700

    Update download_results.sh

--- download_results.sh
+++ download_results.sh
@@ -1,6 +1,6 @@
 pip install gdown
 mkdir results
 cd results
-gdown --folder https://drive.google.com/drive/folders/1DV1WN3aYCO6w1sDHA_c3O1lT3bByjG4m
+gdown --folder https://drive.google.com/drive/folders/0B0R-k5HYb2OVflZCYUFaX1N4WEZPZ2YzSWhVQjIyWU9sUzhaTjFUVkpocE1uc1dIcVo5blk?resourcekey=0-_Yeftuf9jXcnifeoWTfS7g&usp=sharing
 unzip *.zip
 rm *.zip
