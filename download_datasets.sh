cae959ee33b66a0dc09acf90d8dfb8b65bccf8c6
owenzlz <32859275+owenzlz@users.noreply.github.com>
2023-11-04 12:56:39 -0700

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,6 +1,6 @@
 pip install gdown
 mkdir datasets
 cd datasets
-gdown https://drive.google.com/uc?id=1R7sDZzz25xKello36V-S4-xkw4pjCX6O
+gdown https://drive.google.com/file/d/1w4qwHhDb25r0RIWwPjgOp86G6SQnnBMO/view?usp=sharing
 unzip SuperCAF_testset.zip
 rm SuperCAF_testset.zip
